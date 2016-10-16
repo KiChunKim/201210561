35092cd51713c25c244e25a1ae2ce82de953f2ce
201210561 <cn0207@naver.com>
2016-10-17 01:05:31 +0900

    Add files via upload

---
+++ data_rest_p1.py
@@ -0,0 +1,25 @@
+﻿import os 
+KEY="697a486559636e3037304356486844" 
+TYPE='json'
+SERVICE = 'StationAdresTelno' 
+START_INDEX='1'
+END_INDEX='20'
+LINE_NUM='2'
+
+url='http://openapi.seoul.go.kr:8088/'
+url+=KEY
+url+='/'
+url+=TYPE
+url+='/'
+url+=SERVICE
+url+='/'
+url+=START_INDEX
+url+='/'
+url+=END_INDEX
+url+='/'
+url+=LINE_NUM
+
+import requests
+data=requests.get(url)
+
+print data.text
